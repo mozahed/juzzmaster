bc8d1b7cc69f3ed9a05767dccd16d371203d6d40
hello <hello@gmail.com>
2018-05-13 15:01:14 +0600

    jazz master

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	
+	$(".slider-main").owlCarousel({
+		items: 1,
+		loop: true,
+		//nav: true,
+		//navText: ['<i class="fa fa-angle-right"></i>','<i class="fa fa-angle-left"></i>'],
+		autoplay: true,
+		mouseDrag: true,
+		autoplayTimeout: 1000,
+		autoplayHoverPause: true,
+		//dot:true;
+	});
+	
+});
